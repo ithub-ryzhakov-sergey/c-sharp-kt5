0c1492be921c3208b2da1072825b5ff52ade9b32
Danya <dansteamer1337@gmail.com>
2025-09-27 13:25:24 +0300

    very well

--- App/Topics/InterfacePropertiesIndexers/T3_1_PersonDirectory/Directory.cs
+++ App/Topics/InterfacePropertiesIndexers/T3_1_PersonDirectory/Directory.cs
@@ -4,3 +4,43 @@
 
 namespace App.Topics.InterfacePropertiesIndexers.T3_1_PersonDirectory;
 
+public interface IPersonDirectory
+{
+    int Count { get; }
+    string this[int id] { get; set; }
+}
+public class InMemoryPersonDirectory : IPersonDirectory
+{
+    private Dictionary<int, string> storage = new Dictionary<int, string>();
+    public int Count
+    {
+        get{ return storage.Count; }
+
+    }
+    public string this[int id]
+    {
+        get
+        {
+            if (id < 0) {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            if (storage.ContainsKey(id)) {
+                return storage[id];
+            }
+            else {
+                throw new KeyNotFoundException(nameof(id));
+            }
+        }
+        set
+        {
+            if (id < 0) {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            if (string.IsNullOrWhiteSpace(value)) {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            storage[id] = value;
+        }
+    }
+}
--- App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
+++ App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
@@ -5,3 +5,53 @@
 
 namespace App.Topics.Interfaces.T1_1_BasicShapes;
 
+public interface IShape
+{
+    string Name { get; }
+    double Area();
+}
+public class Rectangle : IShape
+{
+    public double Width { get; }
+    public double Height { get; }
+    public string Name => "Rectangle";
+
+    public double Area()
+    {
+        return Width * Height;
+    }
+    public Rectangle(double width, double height)
+    {
+        if (width <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(width), "Ширина должна быть положительной");
+        }
+
+        if (height <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(height), "Высота должна быть положительной");
+        }
+
+        Width = width;
+        Height = height;
+    }
+}
+public class Circle : IShape
+{
+    public double Radius { get; }
+    public string Name => "Circle";
+
+    public double Area()
+    {
+        return Math.PI * Radius * Radius;
+    }
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(radius));
+        }
+
+        Radius = radius;
+    }
+}
