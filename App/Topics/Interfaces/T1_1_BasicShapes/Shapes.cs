f3826d5e076a4780b13f5d7dc466805819c991e7
staro <staro@MIRODEMPC>
2025-09-27 12:48:42 +0300

    мпавпвыап

--- App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
+++ App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
@@ -3,5 +3,42 @@
 // В этом файле описан интерфейс IShape и заготовки классов фигур.
 // Ваша задача — реализовать проверки, свойства и методы согласно README.
 
+
 namespace App.Topics.Interfaces.T1_1_BasicShapes;
 
+public interface IShape
+{
+    string Name { get; }    
+    double Area();
+}
+
+public class Rectangle : IShape
+{
+    public double Width { get; }
+    public double Height { get; }
+
+    public Rectangle(double width, double height)
+    {
+        if (width <= 0) throw new ArgumentOutOfRangeException(nameof(width));
+        if (height <= 0) throw new ArgumentOutOfRangeException(nameof(height));
+
+        Width = width;
+        Height = height;
+    }
+
+    public string Name => "Rectangle";
+    public double Area() => Width * Height ;
+}
+
+public class Circle : IShape
+{
+    public double Radius { get; }
+    public Circle(double radius)
+    {
+        if (radius <= 0) throw new ArgumentOutOfRangeException(nameof(radius));
+        Radius = radius;
+    }
+    public string Name => "Circle";
+    public double Area() => Math.PI * Radius * Radius;
+}
+ 
--- App/Topics/Interfaces/T1_2_CompositeLogger/Logging.cs
+++ App/Topics/Interfaces/T1_2_CompositeLogger/Logging.cs
@@ -3,5 +3,83 @@
 // Реализуйте интерфейс ILogger и классы MemoryLogger и CompositeLogger согласно README.
 // Оставьте NotImplementedException — тесты должны падать, пока студент не реализует решение.
 
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace App.Topics.Interfaces.T1_2_CompositeLogger;
+
+public interface ILogger 
+{
+    void Log(string message); void Warn(string message); void Error(string message);
+}
+
+public class MemoryLogger : ILogger
+{
+    public List<string> Info { get; } = new List<string>();
+    public List<string> Warnings { get; } = new List<string>();
+    public List<string> Errors { get; } = new List<string>();
+
+    public void Log(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        Info.Add(message);
+    }
+
+    public void Warn(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        Warnings.Add(message);
+    }
+    public void Error(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        Errors.Add(message);
+    }
+}
+
+public class CompositeLogger : ILogger 
+{   
+    private readonly IEnumerable<ILogger> _loggers;
+
+    public CompositeLogger(IEnumerable<ILogger> loggers)
+    {
+        if (loggers == null) throw new ArgumentNullException(nameof(loggers));
+
+        var validLogger = new List<ILogger>();
+        foreach(var logger in loggers)
+        {
+            if (logger != null)
+            {
+                validLogger.Add(logger);
+            }
+        }
+
+        _loggers = validLogger;
+    }
+
+    public void Log(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        foreach (var logger in _loggers)
+        {
+            logger.Log(message);
+        }
+    }
+
+    public void Warn(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        foreach (var logger in _loggers)
+        {
+            logger.Warn(message);
+        }
+    }
+    public void Error(string message)
+    {
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        foreach (var logger in _loggers)
+        {
+            logger.Error(message);
+        }
+    }
+}
 
