1ab1824d8939053f28c45deccd26ead49bf2bc38
n1k1tal0x <d.cuzya07@gmail.com>
2025-09-27 12:05:56 +0300

    T1.1 - ou yes T2.1 - not full

--- App/Topics/InterfaceReferences/T2_1_ZooSpeak/Zoo.cs
+++ App/Topics/InterfaceReferences/T2_1_ZooSpeak/Zoo.cs
@@ -4,4 +4,27 @@
 
 namespace App.Topics.InterfaceReferences.T2_1_ZooSpeak;
 
+interface IAnimal
+{
+    string Speak();
+}
 
+public class Dog: IAnimal {
+    public string Speak()
+    {
+        return "woof";
+    }
+}
+public class Cat: IAnimal {
+    public string Speak()
+    {
+        return "meow";
+    }
+}
+public class Duck : IAnimal
+{
+    public string Speak()
+    {
+        return "quack";
+    }
+}
