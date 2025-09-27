52800f78c69bfca70aba6c8d06a52b7c170a1f23
user1 <user1@DESKTOP-ENRJJP4>
2025-09-27 12:00:46 +0300

    task 1

--- App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
+++ App/Topics/Interfaces/T1_1_BasicShapes/Shapes.cs
@@ -5,3 +5,52 @@
 
 namespace App.Topics.Interfaces.T1_1_BasicShapes;
 
+public interface IShape
+{
+    string Name { get; }
+    double Area();
+}
+
+public class Rectangle : IShape
+{
+    public double Width { get; }
+    public double Height { get; }
+    public string Name => "Rectangle";
+
+    public Rectangle(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+
+        if (width <= 0)
+            throw new ArgumentOutOfRangeException(nameof(width));
+        if (height <= 0)
+            throw new ArgumentOutOfRangeException(nameof(height));
+    }
+
+    public double Area()
+    {
+        return Width * Height;
+    }
+}  
+
+public class Circle : IShape
+{
+    public double Radius { get; }
+    public string Name => "Circle";
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+
+        if (radius <= 0)
+            throw new ArgumentOutOfRangeException(nameof(radius));
+    }
+
+    public double Area()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+
