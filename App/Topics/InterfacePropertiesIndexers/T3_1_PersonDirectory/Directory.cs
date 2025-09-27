04bae7660fb477978c0a278a8b341b6a792d0004
user1 <user1@DESKTOP-ODQ5KOQ>
2025-09-27 13:09:00 +0300

    Бокшудаев

--- App/Topics/InterfacePropertiesIndexers/T3_1_PersonDirectory/Directory.cs
+++ App/Topics/InterfacePropertiesIndexers/T3_1_PersonDirectory/Directory.cs
@@ -1,6 +1,37 @@
-// Topic 3: Interface Properties and Indexers
-// Task T3.1 PersonDirectory (обязательная)
-// Реализуйте интерфейс IPersonDirectory и класс InMemoryPersonDirectory согласно README.
-
 namespace App.Topics.InterfacePropertiesIndexers.T3_1_PersonDirectory;
 
+public interface IPersonDirectory
+{
+    int Count { get; }
+    string this[int id] { get; set; }
+}
+
+public class InMemoryPersonDirectory : IPersonDirectory
+{
+    private Dictionary<int, string> _storage = new Dictionary<int, string>();
+
+    public int Count => _storage.Count;
+
+    public string this[int id]
+    {
+        get
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "OutOfRangeException");
+            if (!_storage.TryGetValue(id, out var name))
+                throw new KeyNotFoundException($"ID {id} нет данных");
+            return name;
+        }
+        set
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "OutOfRangeException");
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value), "нет данных");
+
+            _storage[id] = value;
+        }
+    }
+}
--- App/Topics/InterfaceReferences/T2_1_ZooSpeak/Zoo.cs
+++ App/Topics/InterfaceReferences/T2_1_ZooSpeak/Zoo.cs
@@ -1,8 +1,35 @@
-// Topic 2: Interface References
-// Task T2.1 ZooSpeak (обязательная)
-// Реализуйте интерфейс IAnimal, классы животных и функцию SpeakAll согласно README.
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace App.Topics.InterfaceReferences.T2_1_ZooSpeak;
+public interface IAnimal
+{
+    string Speak();
+}
 
-public interface IAnimal;
+public class Dog : IAnimal
+{
+    public string Speak() => "woof";
+}
 
+public class Cat : IAnimal
+{
+    public string Speak() => "meow";
+}
+
+public class Duck : IAnimal
+{
+    public string Speak() => "quack";
+}
+
+public static class ZooUtils
+{
+    public static string[] SpeakAll(IEnumerable<IAnimal> animals)
+    {
+        if (animals == null)
+            throw new ArgumentNullException(nameof(animals));
+        if (animals.Any(a => a == null))
+            throw new ArgumentNullException(nameof(animals));
+        return animals.Select(a => a.Speak()).ToArray();
+    }
+}
